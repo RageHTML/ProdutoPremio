e51277db862aa741b227f80e96e3987b25fb5488
RageHTML <deyvidm24@gmail.com>
2026-07-26 22:46:58 +0000

    feat() adicioanando botao de logout protegido com csrf token para evitar post indesejados, adiconando beautifulsoup4 para webscraping:

--- ProdutoPremio/myprofile/urls.py
+++ ProdutoPremio/myprofile/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("myprofile/", views.carregar_profile, name="profile_usuario"),
+    path("logout/", views.logout_usuario, name="logout")
 ]
--- ProdutoPremio/myprofile/views.py
+++ ProdutoPremio/myprofile/views.py
@@ -1,7 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
-from reglog.models import Usuarios
-from django.contrib.auth.models import User
+from django.contrib.auth import logout
 
 
 def carregar_profile(request):
@@ -11,3 +10,10 @@ def carregar_profile(request):
         else:
             username = request.user.get_username()
             return render(request, "profile/profile.html",{'username': username})
+
+def logout_usuario(request):
+    if request.method == "GET":
+        return redirect("/profile/myprofile")
+    elif request.method == "POST":
+        logout(request) 
+        return redirect('login') 
